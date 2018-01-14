9521ce1c7a4f63defb7ff297d8423b5a40df6c7e
Mounika Bommakanti <mounika10.bommakanti@gmail.com>
2018-01-14 21:42:18 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+function notequal()
+{
+var number=10;
+if(number==10)
+  console.log(number);
+else
+  console.log("not 10");}
+  
+  names=["mounika","preethi","priya"]
+  for(i=0;i<names.length;i++)
+  {
+    console.log(names[i]);
+  }
